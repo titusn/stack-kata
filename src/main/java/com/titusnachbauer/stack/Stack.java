d73c8d742e839f014cf6236c795f7770ebc25ba9
titusn <mail@titusnachbauer.com>
2021-02-14 08:25:32 +0100

    Add push method

--- src/main/java/com/titusnachbauer/stack/Stack.java
+++ src/main/java/com/titusnachbauer/stack/Stack.java
@@ -1,7 +1,13 @@
 package com.titusnachbauer.stack;
 
 public class Stack {
+    private boolean isEmpty = true;
+
     public boolean isEmpty() {
-        return true;
+        return isEmpty;
+    }
+
+    public void push(int i) {
+        isEmpty = false;
     }
 }
