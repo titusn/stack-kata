3c397d4b93c7543079ece92c628fa3104454d6bb
titusn <mail@titusnachbauer.com>
2021-02-14 08:23:06 +0100

    Refactor and clean up

--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -1,12 +1,13 @@
 import com.titusnachbauer.stack.Stack;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class StackTest {
+    private Stack stack = new Stack();
 
     @Test
     public void NewStackShouldBeEmpty() {
-        Stack stack = new Stack();
-        Assertions.assertTrue(stack.isEmpty());
+        assertTrue(stack.isEmpty());
     }
 }
