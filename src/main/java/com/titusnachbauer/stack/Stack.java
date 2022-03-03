a90205740b95400666f4a08a95f46d372ca343f8
Titus Nachbauer <mail@titusnachbauer.com>
2022-03-03 11:49:08 +0000

    Restart kata

--- src/main/java/com/titusnachbauer/stack/Stack.java
+++ src/main/java/com/titusnachbauer/stack/Stack.java
@@ -1,40 +1,5 @@
 package com.titusnachbauer.stack;
 
 public class Stack {
-    private int count = 0;
-    private int value;
-    private int[] values;
-
-    Stack(int size) {
-        this.values = new int[size];
-    }
-
-    Stack() {
-        this(2);
-    }
-
-    int size() {
-        return values.length;
-    }
-    
-    boolean isEmpty() {
-        return count == 0;
-    }
-
-    int pop() {
-        if (isEmpty()) {
-            throw new Underflow();        
-        }
-        count--;
-        return values[count];
-    }
-
-    void push(int value) {
-        if (count == values.length) {
-            throw new Overflow();
-        } 
-        this.values[count] = value;
-        count++;  
-    }
 
 }
